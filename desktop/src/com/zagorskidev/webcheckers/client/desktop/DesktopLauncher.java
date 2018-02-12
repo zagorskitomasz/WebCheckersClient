26944bd0bcd0712080b494c75715e3ba63611481
Tomek <zagorskitomasz@gmail.com>
2018-02-12 19:50:32 +0100

    fresh libgdx project

---
+++ desktop/src/com/zagorskidev/webcheckers/client/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zagorskidev.webcheckers.client.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zagorskidev.webcheckers.client.WebCheckers;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WebCheckers(), config);
+	}
+}
---
+++ html/src/com/zagorskidev/webcheckers/client/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.zagorskidev.webcheckers.client.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.zagorskidev.webcheckers.client.WebCheckers;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WebCheckers();
+        }
+}
